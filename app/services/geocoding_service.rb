8f53e2b3552bcd911fbdd7273b2e223d76439d45
brendanbenson <brendanbenson@gmail.com>
2026-02-08 11:28:42 -0700

    Refactors geocoding_service.rb

--- app/services/geocoding_service.rb
+++ app/services/geocoding_service.rb
@@ -13,12 +13,13 @@ class GeocodingService
   # @param [String] address The address to be geocoded
   def location(address:)
     Rails.cache.fetch(cache_key(address), expires_in: 1.week) do
-      result_loc = result_location(address: address)
-      country = country(address: address)
+      geocode_result = @client.geocode_address(address: address).body.as_json
+      result_loc = result_location(geocode_result)
+      country = country(geocode_result)
       location = Location.new(
         latitude: result_loc.fetch("lat"),
         longitude: result_loc.fetch("lng"),
-        postal_code: postal_code(address: address),
+        postal_code: postal_code(geocode_result),
         country: country
       )
       raise GeocodingServiceError unless location.valid?
@@ -36,30 +37,25 @@ class GeocodingService
     address.downcase.gsub(/\s+/, " ").strip
   end
 
-  # @param [String] address
+  # @param [Hash] geocode_result
   # @return [String]
-  def postal_code(address:)
-    geocoded_address(address: address).dig("results", 0, "address_components")
+  def postal_code(geocode_result)
+    geocode_result.dig("results", 0, "address_components")
       &.find { |component| component["types"].include?("postal_code") }&.dig("long_name")
   end
 
-  # @param [String] address
+  # @param [Hash] geocode_result
   # @return [String]
-  def country(address:)
-    geocoded_address(address: address).dig("results", 0, "address_components")
+  def country(geocode_result)
+    geocode_result.dig("results", 0, "address_components")
       &.find { |component| component["types"].include?("country") }&.dig("short_name")
   end
 
-  # @param [String] address
+  # @param [Hash] geocode_result
   # @return [Hash]
-  def result_location(address:)
-    result = geocoded_address(address: address).dig("results", 0, "geometry", "location")
+  def result_location(geocode_result)
+    result = geocode_result.dig("results", 0, "geometry", "location")
     raise GeocodingServiceError if result.nil?
     result
-  end
-
-  # @param [String] address
-  def geocoded_address(address:)
-    @geocoded_address ||= @client.geocode_address(address: address).body.as_json
   end
 end
